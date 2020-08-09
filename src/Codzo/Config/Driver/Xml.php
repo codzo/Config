f63021cdaddba71c97835ca7581adcfec72f7030
neilfan <neil.fan@codzo.com>
2020-08-09 20:26:30 +1000

    simplify XML driver

--- src/Codzo/Config/Driver/Xml.php
+++ src/Codzo/Config/Driver/Xml.php
@@ -1,10 +1,12 @@
 <?php
+
 /**
  * Codzo/Config/Driver/Xml.php
  * @author Neil Fan<neil.fan@codzo.com>
  * @version GIT: $Id$
  * @package Codzo\Config
  */
+
 namespace Codzo\Config\Driver;
 
 use Codzo\Config\Driver\AbstractDriver;
@@ -20,13 +22,12 @@ class Xml extends AbstractDriver
     public function parse($file_path)
     {
         $data = array();
-        $xml = @simplexml_load_file($file_path);
+        // Note: we do not use `simplexml_load_file()`
+        // because of https://bugs.php.net/bug.php?id=62577
+        $xml = simplexml_load_string(file_get_contents($file_path));
 
         if ($xml instanceof \SimpleXMLElement) {
-            $data[$xml->getName()] = json_decode(
-                json_encode((array)$xml),
-                1
-            );
+            $data[$xml->getName()] = (array)$xml;
         }
 
         if (!$data) {
